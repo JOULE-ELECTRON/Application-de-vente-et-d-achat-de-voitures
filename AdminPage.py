4704b5747cf0878d337c535f0910b4fdfd743565
Diae-Eddine2 <diaeeddine.frij@centrale-casablanca.ma>
2024-10-10 16:12:54 +0200

    Test 1
    test1

---
+++ AdminPage.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
+import clients
+
+class Car:
+    def __init__(self, marque, modele, description, prix, image_path):
+        self.marque = marque
+        self.modele = modele
+        self.description = description
+        self.prix = prix
+        self.image_path = image_path
